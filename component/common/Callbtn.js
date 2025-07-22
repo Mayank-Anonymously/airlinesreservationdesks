844d820b6debb6a68c91bd7ab5dd7a79b051bf64
acedeveloper-byte <mayank.kumarace@gmail.com>
2025-07-22 15:01:57 +0530

    Update Callbtn.js
    Number change

--- component/common/Callbtn.js
+++ component/common/Callbtn.js
@@ -9,7 +9,7 @@ const CallNowBanner = () => {
       </div>
       <div className="call-text">
         <span className="call-now">CALL US NOW</span>
-        <span className="phone-number"><a href="tel:+1(888) 888-8888" className='text-decoration-none text-white'>+1(888) 888-8888</a></span>
+        <span className="phone-number"><a href="tel:++1(855) 236-1030" className='text-decoration-none text-white'>+1(855) 236-1030</a></span>
       </div>
     </div>
   );
